ea27265a2959064347d0357ff2773bab02ff1321
Sriharish <harish872004@gmail.com>
2025-09-07 11:45:05 +0530

    Security Check

--- src/main/java/sri/cloudshareapi/config/SecurityConfig.java
+++ src/main/java/sri/cloudshareapi/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://cloudshareapp.netlify.app/"));
+        config.setAllowedOrigins(List.of("https://cloudshareapp.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
